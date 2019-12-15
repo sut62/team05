444dcd927c00d0513792a8ff075e3fe4ec8fe08b
vongola717 <vongola717@gmail.com>
2019-12-15 18:02:03 +0700

    Borrow controller - close #15

---
+++ backend/src/main/java/com/cpe/backend/borrow/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.cpe.backend.borrow.controller;
+
+import com.cpe.backend.borrow.entity.Employee;
+import com.cpe.backend.borrow.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class EmployeeController {
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/employee")
+    public Collection<Employee> Employees() {
+        return employeeRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
