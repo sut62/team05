d914668b3478a4bd8ed477004e425736aec5be40
Kittiphun-Arm <arm68276728@gmail.com>
2019-12-28 22:02:29 +0700

    close #32 - ทำ Controller ในส่วนของ backend

---
+++ backend/src/main/java/com/cpe/backend/Members/controller/ProvinceController.java
@@ -0,0 +1,38 @@
+package com.cpe.backend.Members.controller;
+
+import com.cpe.backend.Members.entity.Province;
+import com.cpe.backend.Members.repository.ProvinceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class ProvinceController {
+
+    @Autowired
+    private final ProvinceRepository provinceRepository;
+
+    public ProvinceController(ProvinceRepository provinceRepository) {
+        this.provinceRepository = provinceRepository;
+    }
+
+    @GetMapping("/province")
+    public Collection<Province> Province() {
+        return provinceRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping("/province/{id}")
+    public Optional<Province> Province(@PathVariable Long id) {
+        Optional<Province> province = provinceRepository.findById(id);
+        return province;
+    }
+
+}
