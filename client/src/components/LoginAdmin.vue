8d5d9a298b9bd728911e380e0e75b3f3f74ea4e2
Bell-Thanet <bellj2541@hotmail.com>
2020-01-09 23:21:40 +0700

    close #65 - UI Logingเจ้าของสถานกีฬา

---
+++ client/src/components/LoginAdmin.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-container>
+    <br />
+    <br />
+    <br />
+    <v-card max-width="700" class="mx-auto">
+      <v-system-bar color="green"></v-system-bar>
+      <v-system-bar color="green"></v-system-bar>
+      <v-row justify="center">
+        <v-toolbar-title>
+          <h1>Login</h1>
+        </v-toolbar-title>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="5">
+          <v-text-field
+            solo
+            label="Email"
+            v-model="email"
+            :rules="[(v) => !!v || 'This field is required']"
+            required
+            clearable
+            prepend-icon="mdi-email"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col cols="5">
+          <v-text-field
+            solo
+            label="PASSWORD"
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            required
+            counter
+            clearable
+            @click:append="show = !show"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col>
+          <v-btn style="margin-left:43%;" @click="BLogin">Login</v-btn>
+
+          <!-- <v-btn 
+                style="margin-left:25%;"
+                @click="Register">Register
+                 
+          </v-btn>-->
+        </v-col>
+      </v-row>
+      <br />
+      <br />
+      <v-system-bar color="green"></v-system-bar>
+      <v-system-bar color="green"></v-system-bar>
+    </v-card>
+  </v-container>
+</template>
+
+
+
+<script>
+import http from "../http-common";
+export default {
+  name: "Loing",
+  data() {
+    return {
+      email: "",
+      password: "",
+      show: false
+    };
+  },
+  methods: {
+    getUsers() {
+      http
+        .get("/user")
+        .then(response => {
+          this.users = response.data;
+
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    BLogin() {
+      this.$router.push("/Adminmenu");
+      http
+        .get("/checkuser/" + this.email + "/" + this.password)
+        .then(response => {
+          console.log(response);
+          if (response.data[0] != null) {
+            localStorage.setItem("email", response.data[0].email);
+            localStorage.setItem("name", response.data[0].name);
+            localStorage.setItem("id", response.data[0].id);
+            localStorage.setItem("phone", response.data[0].phone);
+            localStorage.setItem("nametype", response.data[0].nametype);
+            this.$router.push("/Adminmenu");
+          } else {
+            alert("รหัสผ่านไม่ถูกต้อง");
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+    // Register(){
+    //   this.$router.push("/registeruser");
+    // }
+  }
+};
+</script>
