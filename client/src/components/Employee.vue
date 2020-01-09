6e1e38b7407c4ff3b8006f96e85bec83b7d22370
Attakorn66 <attakorn_6062@hotmail.co.th>
2020-01-09 22:53:16 +0700

    close #25 ทำUIของสมัครพนักงาน

---
+++ client/src/components/Employee.vue
@@ -0,0 +1,279 @@
+
+<template>
+  <v-card class="mx-auto" height="100%" width="100%">
+    <v-navigation-drawer
+      absolute
+      dark
+      src="https://c.wallhere.com/photos/e4/53/1920x1080_px_anime_Der_Wanderer_ber_Dem_Nebelmeer_nature_sky_sunset-609196.jpg!d"
+      width="100%"
+      permanent
+    >
+      <br />
+      <v-card class="mx-auto" max-width="850" color="#8B636C">
+        <v-system-bar color="#CD919E"></v-system-bar>
+        <v-system-bar color="#CD919E"></v-system-bar>
+        <v-row justify="center">
+          <v-toolbar-title>
+            <h1>สมัครพนักงาน</h1>
+          </v-toolbar-title>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="10">
+            <v-form>
+              <v-row justify="center">
+                <!-- ชื่อ-สกุล-->
+                <v-col cols="7">
+                  <v-text-field
+                    solo
+                    label="ชื่อ-สกุล"
+                    v-model="name"
+                    :rules="[(v) => !!v || 'This field is required']"
+                    required
+                    clearable
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <!-- ตำแหน่ง -->
+                <v-col cols="5">
+                  <v-select
+                    label="ตำแหน่ง"
+                    solo
+                    v-model="Employee.position"
+                    :items="position"
+                    item-text="position"
+                    item-value="position_id"
+                    :rules="[(v) => !!v || 'Item is required']"
+                    required
+                  ></v-select>
+                </v-col>
+                <!-- จังหวัดที่ตั่ง -->
+                <v-col cols="4">
+                  <v-select
+                    label="จังหวัด"
+                    solo
+                    v-model="Employee.province"
+                    :items="province"
+                    item-text="province"
+                    item-value="province_id"
+                    :rules="[(v) => !!v || 'Item is required']"
+                    required
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <!-- ประเภทโทรศัพท์ -->
+
+                <v-col cols="3">
+                  <v-select
+                    label="ประเภทโทรศัพท์"
+                    solo
+                    v-model="Employee.phonetype"
+                    :items="phonetype"
+                    item-text="phonetype"
+                    item-value="phonetype_id"
+                    :rules="[(v) => !!v || 'Item is required']"
+                    required
+                  ></v-select>
+                </v-col>
+                <!--เบอร์โทรศัพท์-->
+                <v-col cols="5">
+                  <v-text-field
+                    solo
+                    label="เบอร์โทรศัพท์"
+                    v-model="phone"
+                    :rules="[(v) => !!v || 'This field is required']"
+                    required
+                    clearable
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <!-- email -->
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-text-field
+                    solo
+                    label="Email"
+                    v-model="email"
+                    :rules="[(v) => !!v || 'This field is required']"
+                    required
+                    clearable
+                    prepend-icon="mdi-email"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <!-- password -->
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-text-field
+                    solo
+                    label="PASSWORD"
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :phonetype="show1 ? 'text' : 'password'"
+                    hint="At least 8 characters"
+                    prepend-icon="mdi-lock"
+                    required
+                    counter
+                    clearable
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <!-- ยืมยันpassword -->
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-text-field
+                    solo
+                    label="ยืนยัน PASSWORD"
+                    v-model="repassword"
+                    :phonetype="show2 ? 'text' : 'password'"
+                    :append-icon="show2 ?  'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required,rules.checkpass]"
+                    hint="At least 8 characters"
+                    prepend-icon="mdi-lock"
+                    required
+                    counter
+                    clearable
+                    @click:append="show2 = !show2"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row justify="center" style="height: 20px;">
+                <v-col cols="15">
+                  <v-btn style="margin-left: 25%;" @click="saveData">save</v-btn>
+                  <v-btn style="margin-left:25%;" @click="BLogin">Login</v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-col>
+        </v-row>
+        <br />
+        <br />
+        <v-system-bar color="#CD919E"></v-system-bar>
+        <v-system-bar color="#CD919E"></v-system-bar>
+      </v-card>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+<script>
+import http from "../http-common";
+
+export default {
+  name: "Employee",
+  data() {
+    return {
+      Employee: {
+        phonetype: null,
+        position: null,
+        province: null
+      },
+      show1: false,
+      show2: false,
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      repassword: "",
+      position: null,
+      phonetype: null,
+      province: null,
+      rules: {
+        required: value => !!value || "This field is required",
+        min: v => v.length >= 8 || "Min 8 characters",
+        checkpass: () =>
+          this.repassword == this.password || "Passwords do not match"
+      }
+    };
+  },
+  methods: {
+    /* eslint-disable no-console */
+
+    // ดึงข้อมูล position ใส่ combobox
+    getposition() {
+      http
+        .get("/position")
+        .then(response => {
+          this.position = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    // ดึงข้อมูล phonetype ใส่ combobox
+    getphonetype() {
+      http
+        .get("/phonetype")
+        .then(response => {
+          this.phonetype = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    // ดึงข้อมูล province ใส่ combobox
+    getProvince() {
+      http
+        .get("/province")
+        .then(response => {
+          this.province = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    saveData() {
+      if (
+        this.password == this.repassword &&
+        this.password.length >= 8 &&
+        this.repassword.length >= 8
+      ) {
+        http
+          .post(
+            "/Employee/" +
+              this.name +
+              "/" +
+              this.email +
+              "/" +
+              this.password +
+              "/" +
+              this.phone +
+              "/" +
+              this.Employee.position +
+              "/" +
+              this.Employee.phonetype +
+              "/" +
+              this.Employee.province
+          )
+          .then(response => {
+            console.log(response);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+        this.submitted = true;
+        //this.$router.push("/registerEmployee/login");
+        alert("สมัครสมาชิกสำเร็จ");
+      } else {
+        alert("สมัครสมาชิกไม่สำเร็จ");
+      }
+    },
+    BLogin() {
+      this.$router.push("/registerEmployee/login");
+    }
+  },
+  mounted() {
+    this.getposition();
+    this.getphonetype();
+    this.getProvince();
+  }
+};
+</script>
