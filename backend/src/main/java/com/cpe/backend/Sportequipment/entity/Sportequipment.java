334bf5907f2b67f60f174defc07f4396bdba6eac
KwanB <horizonbot1@gmail.com>
2020-01-21 15:24:17 +0700

    close #96 - แก้ไขโค้ดบางส่วนในEntityของSportequipment

--- backend/src/main/java/com/cpe/backend/Sportequipment/entity/Sportequipment.java
+++ backend/src/main/java/com/cpe/backend/Sportequipment/entity/Sportequipment.java
@@ -7,18 +7,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import java.sql.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 
 import com.cpe.backend.Employee.entity.Employee;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Data
@@ -30,13 +29,15 @@ public class Sportequipment {
     @SequenceGenerator(name = "Sportequipment_seq", sequenceName = "Sportequipment_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Sportequipment_seq")
 
-    private @NonNull Long id;
+    private Long id;
 
-   
-    private @NonNull String brand;
-    private @NonNull Long price;
-    private @NonNull Date date;
-    private @NonNull String se_name;
+    @Size(min = 3,max = 15)
+    @NotNull
+    @Pattern(regexp = "[a-zA-Z]{1,20}$")
+    private String brand;
+    private Long price;
+    private Date date;
+    private String se_name;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Category.class)
     @JoinColumn(name = "Category_ID", insertable = true)
