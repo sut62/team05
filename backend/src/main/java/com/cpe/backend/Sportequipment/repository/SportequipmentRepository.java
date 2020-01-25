7f3878d70f95c3261860695d346c51bd9c0b9e5b
KwanB <horizonbot1@gmail.com>
2020-01-26 05:38:47 +0700

    close #105 - แก้ไขโค้ดบางส่วนในRepository ของ Sportequipment

--- backend/src/main/java/com/cpe/backend/Sportequipment/repository/SportequipmentRepository.java
+++ backend/src/main/java/com/cpe/backend/Sportequipment/repository/SportequipmentRepository.java
@@ -2,11 +2,13 @@ package com.cpe.backend.Sportequipment.repository;
 
 import com.cpe.backend.Sportequipment.entity.Sportequipment;
 
+import java.util.Collection;
 import java.util.List;
 
 import com.cpe.backend.Sportequipment.entity.Category;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -15,4 +17,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface SportequipmentRepository extends JpaRepository<Sportequipment, Long> {
     Sportequipment findById(long id);
     public List<Sportequipment> findByCategory(Category category);
+
+   
+
+    @Query(value ="SELECT * FROM Sportequipment s WHERE s.CATEGORY_ID= CATEGORY_ID", nativeQuery = true)
+    Collection<Sportequipment> findCarByPlate(@Param("CATEGORY_ID") String CATEGORY_ID);
 }
