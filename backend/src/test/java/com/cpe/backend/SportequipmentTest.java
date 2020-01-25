ffaf29f8fdec45a5bd6ac75523648fc398dfc5c7
KwanB <horizonbot1@gmail.com>
2020-01-26 05:49:47 +0700

    close #79 - เพิ่มและแก้ไขโค้ดของ Test Entity Sportequipment ในทุกfield

--- backend/src/test/java/com/cpe/backend/SportequipmentTest.java
+++ backend/src/test/java/com/cpe/backend/SportequipmentTest.java
@@ -34,10 +34,10 @@ public class SportequipmentTest {
 //========================================================================================================================
 //brand
 @Test
-void b6020163_testInsertBrandforSportequipmentOK() {
+void b6020163_testSportequipmentInsertFullDataOK() {
     Sportequipment sportequipment = new Sportequipment();
     sportequipment.setBrand("Adidas");
-    sportequipment.setPrice(590);
+    sportequipment.setPrice("590 บาท");
     java.sql.Date date = new java.sql.Date(2020-02-05);
     sportequipment.setDate(date);
     sportequipment.setName("ลูกวอลเลย์บอล");
@@ -45,12 +45,15 @@ void b6020163_testInsertBrandforSportequipmentOK() {
 
     Optional<Sportequipment> found = sportequipmentRepository.findById(sportequipment.getId());
     assertEquals(sportequipment.getBrand(), found.get().getBrand());
+    assertEquals(sportequipment.getPrice(), found.get().getPrice());
+    assertEquals(sportequipment.getDate(), found.get().getDate());
+    assertEquals(sportequipment.getSe_name(), found.get().getSe_name());
 }
 @Test
 void b6020163_testBrandforSportequipmentMustNotBeNull() {
     Sportequipment sportequipment = new Sportequipment();
     sportequipment.setBrand(null);
-    sportequipment.setPrice(590);
+    sportequipment.setPrice("590 บาท");
     java.sql.Date date = new java.sql.Date(2020-02-05);
     sportequipment.setDate(date);
     sportequipment.setName("ลูกวอลเลย์บอล");
@@ -64,7 +67,7 @@ void b6020163_testBrandforSportequipmentMustNotBeNull() {
 void b6020163_testBrandMustBeGreaterEqual3() {
     Sportequipment sportequipment = new Sportequipment();
     sportequipment.setBrand("Ad");
-    sportequipment.setPrice(590);
+    sportequipment.setPrice("590 บาท");
     java.sql.Date date = new java.sql.Date(2020-02-05);
     sportequipment.setDate(date);
     sportequipment.setName("ลูกวอลเลย์บอล");
@@ -78,7 +81,7 @@ void b6020163_testBrandMustBeGreaterEqual3() {
 void b6020163_testBrandMustBeGreaterEqual15() {
     Sportequipment sportequipment = new Sportequipment();
     sportequipment.setBrand("AdidasAdidasAdidas");
-    sportequipment.setPrice(590);
+    sportequipment.setPrice("590 บาท");
     java.sql.Date date = new java.sql.Date(2020-02-05);
     sportequipment.setDate(date);
     sportequipment.setName("ลูกวอลเลย์บอล");
@@ -92,7 +95,7 @@ void b6020163_testBrandMustBeGreaterEqual15() {
     void b6020163_testBrandNotExactlyThePattern() {
     Sportequipment sportequipment = new Sportequipment();
     sportequipment.setBrand("33445515555459");
-    sportequipment.setPrice(590);
+    sportequipment.setPrice("590 บาท");
     java.sql.Date date = new java.sql.Date(2020-02-05);
     sportequipment.setDate(date);
     sportequipment.setName("ลูกวอลเลย์บอล");
@@ -103,6 +106,70 @@ void b6020163_testBrandMustBeGreaterEqual15() {
         assertEquals("must match \"[a-zA-Z]{1,20}$\"", v.getMessage());
         assertEquals("brand", v.getPropertyPath().toString());
     }
+//========================================================================================================================
+//price
+@Test
+void b6020163_testPriceforSportequipmentMustNotBeNull() {
+    Sportequipment sportequipment = new Sportequipment();
+    sportequipment.setBrand("adidas");
+    sportequipment.setPrice(null);
+    java.sql.Date date = new java.sql.Date(2020-02-05);
+    sportequipment.setDate(date);
+    sportequipment.setName("ลูกวอลเลย์บอล");
+    
+    Set<ConstraintViolation<Sportequipment>> result = validator.validate(sportequipment);
+    assertEquals(1, result.size());
+    assertEquals("must not be null", result.iterator().next().getMessage());
+    assertEquals("price", result.iterator().next().getPropertyPath().toString());
+}
 
+//========================================================================================================================
+//date
+@Test
+void b6020163_testDateforSportequipmentMustNotBeNull() {
+    Sportequipment sportequipment = new Sportequipment();
+    sportequipment.setBrand("adidas");
+    sportequipment.setPrice("590 บาท");
+    java.sql.Date date = new java.sql.Date(2020-02-05);
+    sportequipment.setDate(null);
+    sportequipment.setName("ลูกวอลเลย์บอล");
+    
+    Set<ConstraintViolation<Sportequipment>> result = validator.validate(sportequipment);
+    assertEquals(1, result.size());
+    assertEquals("must not be null", result.iterator().next().getMessage());
+    assertEquals("date", result.iterator().next().getPropertyPath().toString());
+}
+
+//========================================================================================================================
+//se_name
+@Test
+void b6020163_testSe_nameforSportequipmentMustNotBeNull() {
+    Sportequipment sportequipment = new Sportequipment();
+    sportequipment.setBrand("adidas");
+    sportequipment.setPrice("590 บาท");
+    java.sql.Date date = new java.sql.Date(2020-02-05);
+    sportequipment.setDate(date);
+    sportequipment.setName(null);
+    
+    Set<ConstraintViolation<Sportequipment>> result = validator.validate(sportequipment);
+    assertEquals(1, result.size());
+    assertEquals("must not be null", result.iterator().next().getMessage());
+    assertEquals("se_name", result.iterator().next().getPropertyPath().toString());
+}
+@Test
+    void b6020163_testSe_nameNotExactlyThePattern() {
+    Sportequipment sportequipment = new Sportequipment();
+    sportequipment.setBrand("adidas");
+    sportequipment.setPrice("590 บาท");
+    java.sql.Date date = new java.sql.Date(2020-02-05);
+    sportequipment.setDate(date);
+    sportequipment.setName("1254698%%%%");
+
+        Set<ConstraintViolation<Sportequipment>> result = validator.validate(sportequipment);
+        assertEquals(1, result.size());
+        ConstraintViolation<Sportequipment> v = result.iterator().next();
+        assertEquals("must match \"[ก-๙ a-zA-Z]*\"", v.getMessage());
+        assertEquals("se_name", v.getPropertyPath().toString());
+    }
 
 }
